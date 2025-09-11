55d921c474eebb68b78a47fa9a0a51db7bc758c9
muhammad rohail <muhammad.rohail17@gmail.com>
2025-09-11 06:41:10 -0400

    hero section

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let listIcon = document.querySelector(".list-icon");
 let sideBar = document.querySelector(".sidebar");
 let underlay = document.querySelector(".underlay");
+let images = document.querySelectorAll(".hero-images");
 
 listIcon.addEventListener("click", () => {
   underlay.style.visibility = "visible";
@@ -11,3 +12,16 @@ underlay.addEventListener("click", () => {
   underlay.style.visibility = "hidden";
   sideBar.style.transform = "translateX(-100%)";
 });
+
+let num = 0;
+setInterval(() => {
+  if (num > images.length - 1) {
+    num = 0;
+  }
+
+  images.forEach((items, index) => {
+    items.style.opacity = "0";
+  });
+  images[num].style.opacity = "1";
+  num++;
+}, 3000);
